8dbf8b142e6afd4189e420e5d9bb106d2a1fa1db
Weeleeam <william.jonnatan@ui.ac.id>
2026-02-08 20:47:55 +0700

    Add Delete Feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -12,11 +12,18 @@ public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        if (product.getProductId() == null) {
+            product.setProductId(java.util.UUID.randomUUID().toString());
+        }
         productData.add(product);
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public boolean delete(String productId) {
+        return productData.removeIf(product -> product.getProductId().equals(productId));
     }
 }
