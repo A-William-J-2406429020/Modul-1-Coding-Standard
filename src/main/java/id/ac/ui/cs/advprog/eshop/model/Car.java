9a5fdfe8ef5b45e4cc26dbc8002f22848e025750
Weeleeam <william.jonnatan@ui.ac.id>
2026-03-03 13:29:00 +0700

    add Car model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.eshop.model;
+import lombok.Setter;
+import lombok.Getter;
+
+public class Car {
+    private String carId;
+    private String carName;
+    private String carColor;
+    private int carQuantity;
+}
